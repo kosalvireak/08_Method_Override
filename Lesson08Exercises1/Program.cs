b579dfc0b81bb9fc12090aed69cffb1ef7cbac45
Kosalvireak Khoeun <virakvary@gmail.com>
2022-08-12 16:36:05 +0700

    commit

---
+++ Lesson08Exercises1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson08Exercises1
+{
+    class ElectronicsShop
+    {
+        private string strShopName;
+        public ElectronicsShop(string strShopName)
+        {
+            this.strShopName = strShopName;
+        }
+        public virtual string GetDiscount()
+        {
+            return "0%";
+        }
+    }
+    class Computer:ElectronicsShop
+    {
+        private string strBrand;
+        public Computer(string strBrand, string strShopName):base(strShopName)
+        {
+            this.strBrand = strBrand;
+        }
+        public override string GetDiscount()
+        {
+            return base.GetDiscount();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
