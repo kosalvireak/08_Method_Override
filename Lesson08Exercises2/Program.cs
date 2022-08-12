efddbeccf77c460918c65549919641278ebe38dc
Kosalvireak Khoeun <virakvary@gmail.com>
2022-08-12 17:02:28 +0700

    add new object

---
+++ Lesson08Exercises2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson08Exercises2
+{
+    class Picture
+    {
+        private string strtitle;
+        public string Title
+        {
+            get { return strtitle; }
+        }
+        private float fltPrice;
+
+        public float Price
+        {
+            get { return fltPrice; }
+            set { fltPrice = value; }
+        }
+        public Picture(string strtitle, float fltPrice)
+        {
+            this.strtitle = strtitle;
+            this.fltPrice = fltPrice;
+        }
+        public virtual float GetPrice()
+        {
+            return fltPrice;
+        }
+    }
+    class Photograph : Picture
+    {
+        private string strPhotographer;
+        private string strCamera;
+        public Photograph(string strPhotographer, string strCamera, string strtitle, float fltPrice) : base(strtitle, fltPrice)
+        {
+            this.strPhotographer = strPhotographer;
+            this.strCamera = strCamera;
+        }
+        public override float GetPrice()
+        {
+            return Price + 30f;
+        }
+    }
+    class Painting : Picture
+    {
+        private string strArtist;
+        public string Artist
+        {
+            get { return strArtist; }
+        }
+        private string strType; //g
+        public string Type
+        {
+            get { return strType; }
+
+        }
+        public Painting(string strArtist, string strType, string strtitle, float fltPrice) : base(strtitle, fltPrice)
+        {
+            this.strArtist = strArtist;
+            this.strType = strType;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
